046f3f278fe32dc6c7799f3eaf266eca28dbb446
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-02-03 10:20:39 +0000

    Update Main.php

--- src/NamelessLeaf/hub/Main.php
+++ src/NamelessLeaf/hub/Main.php
@@ -2,7 +2,24 @@
 
 namespace NamelessLeaf\hub; 
 
+use pocketmine\Plugin;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandSender;
+
+class Main extends PluginBase implements Listener {
+  
+  public fucntion onEnable(){
+    $this->getLogger()->info("Enabled");
+  }
+}
